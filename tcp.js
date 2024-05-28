d8c1c759ff5d1baeb7e28e8af440ace4b0b5d71e
小潼 <110387028+XiaoTong6666@users.noreply.github.com>
2024-05-29 02:16:00 +0800

    Update tcp.js

--- tcp.js
+++ tcp.js
@@ -1,29 +1,42 @@
 const net = require('net');
 const port = process.argv[2];
+
 if (!port) {
   console.error('举个栗子🌰：node tcp.js 11451');
   process.exit(1);
 }
-const server = net.createServer((socket) => {
-  console.log('啊~被入了');
-  socket.on('data', (data) => {
-    process.stdout.write(`弄进来的东西：${data.toString()}`);
+
+try {
+  const server = net.createServer((socket) => {
+    console.log('啊~被入了');
+    socket.on('data', (data) => {
+      process.stdout.write(`弄进来的东西：${data.toString()}`);
+    });
+    socket.on('end', () => {
+      console.log('呜呜呜，出去了');
+    });
+    socket.on('error', (err) => {
+      console.error('嗯？哥哥是用nmap把我入的嘛?呜呜呜为什么不用telnet');
+    });
   });
-  socket.on('end', () => {
-    console.log('呜呜呜，出去了');
+
+  server.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+      console.error(`我的${port}号口已被用了呜呜X﹏X，麻烦哥哥换一个 吧qwq`);
+    } else {
+      console.error('嗯？这里有个错误哥哥要看嘛喵~', err);
+    }
+    process.exit(1);
   });
-  socket.on('error', (err) => {
-    console.error('嗯？哥哥是用nmap把我入的嘛?呜呜呜为什么不用telnet');
+
+  server.listen(port, () => {
+    console.log(`哥哥记住了噢！请对准我的${port}号口`);
   });
-});
-server.on('error', (err) => {
-  if (err.code === 'EADDRINUSE') {
-    console.error(`我的${port}口已被用了呜呜X﹏X，麻烦哥哥换一个吧qwq`);
+} catch (err) {
+  if (err.code === 'ERR_SOCKET_BAD_PORT') {
+    console.error(`${port}太大了，我开不了呜呜呜qwq\n你要记住了！大于65535我都承受不住啦，麻烦哥哥小一点(∠・ω< )⌒☆`);
   } else {
-    console.error('嗯？这里有个错误哥哥要看嘛喵~',err);
+    console.error('嗯？这里有个错误哥哥要看嘛喵~', err);
   }
   process.exit(1);
-});
-server.listen(port, () => {
-  console.log(`哥哥记住了噢！请入我的${port}口`);
-});
+}
